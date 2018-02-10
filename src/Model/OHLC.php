b1d6743790bb5dd37ba867250e3d8906e457265d
Geoffrey Hervet <hervetg@gmail.com>
2018-02-11 00:28:34 +0100

    GSetter

--- src/Model/OHLC.php
+++ src/Model/OHLC.php
@@ -49,4 +49,166 @@ class OHLC
      */
     private $closeDate;
 
+    /**
+     * @return Coin
+     */
+    public function getCoin(): Coin
+    {
+        return $this->coin;
+    }
+
+    /**
+     * @param Coin $coin
+     * @return OHLC
+     */
+    public function setCoin(Coin $coin): OHLC
+    {
+        $this->coin = $coin;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOpen(): int
+    {
+        return $this->open;
+    }
+
+    /**
+     * @param int $open
+     * @return OHLC
+     */
+    public function setOpen(int $open): OHLC
+    {
+        $this->open = $open;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHigh(): int
+    {
+        return $this->high;
+    }
+
+    /**
+     * @param int $high
+     * @return OHLC
+     */
+    public function setHigh(int $high): OHLC
+    {
+        $this->high = $high;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLow(): int
+    {
+        return $this->low;
+    }
+
+    /**
+     * @param int $low
+     * @return OHLC
+     */
+    public function setLow(int $low): OHLC
+    {
+        $this->low = $low;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getClose(): int
+    {
+        return $this->close;
+    }
+
+    /**
+     * @param int $close
+     * @return OHLC
+     */
+    public function setClose(int $close): OHLC
+    {
+        $this->close = $close;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVolume(): int
+    {
+        return $this->volume;
+    }
+
+    /**
+     * @param int $volume
+     * @return OHLC
+     */
+    public function setVolume(int $volume): OHLC
+    {
+        $this->volume = $volume;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMarketCap(): int
+    {
+        return $this->marketCap;
+    }
+
+    /**
+     * @param int $marketCap
+     * @return OHLC
+     */
+    public function setMarketCap(int $marketCap): OHLC
+    {
+        $this->marketCap = $marketCap;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getOpenDate(): \DateTime
+    {
+        return $this->openDate;
+    }
+
+    /**
+     * @param \DateTime $openDate
+     * @return OHLC
+     */
+    public function setOpenDate(\DateTime $openDate): OHLC
+    {
+        $this->openDate = $openDate;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCloseDate(): \DateTime
+    {
+        return $this->closeDate;
+    }
+
+    /**
+     * @param \DateTime $closeDate
+     * @return OHLC
+     */
+    public function setCloseDate(\DateTime $closeDate): OHLC
+    {
+        $this->closeDate = $closeDate;
+        return $this;
+    }
+
 }
